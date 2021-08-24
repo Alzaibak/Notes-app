c0e5208ef1553010e6d5526ef75895e083b024eb
alzaibak <alzaibak.abdalaziz@gmail.com>
2021-08-24 21:04:54 +0200

    add security tto the files

--- connection.php
+++
@@ -1,7 +0,0 @@
-<?php
-$link = mysqli_connect("localhost", "diphantc_alzaibak", "_a59gy!Ej]R#", "diphantc_Online-notes");
-if(mysqli_connect_error()){
-    die('ERROR: Unable to connect:' . mysqli_connect_error()); 
-    echo "<script>window.alert('Hi!')</script>";
-}
-    ?>
