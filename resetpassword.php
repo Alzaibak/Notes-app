ff66541a33f1c0fe0e6ff3d3ddd596ea9df6ece6
alzaibak <alzaibak.abdalaziz@gmail.com>
2021-08-28 22:47:23 +0200

    reset password form

--- resetpassword.php
+++ resetpassword.php
@@ -13,7 +13,11 @@ include('connection.php');
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Password Reset</title>
-        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Arvo:ital@1&display=swap" rel="stylesheet">
         <style>
             h1{
                 color:purple;   
@@ -32,15 +36,109 @@ include('connection.php');
         <div class="col-sm-offset-1 col-sm-10 contactForm">
             <h1>Reset Password:</h1>
             <div id="resultmessage"></div>
+
+    <form action = "resetpassword.php" method="post" id="forgotpasswordform">
+            <div id="forgotpasswordModal">
+            <div>
+                <div>
+                    <div>   
+                    <div>
+
+                        <!--Sign up message from PHP file-->
+                        <div id="forgotpasswordmessage"></div>
+
+                        <div class="form-group">
+                            <label for="email" class="sr-only"></label>
+                            <input class="form-control" type="email" name="forgotpasswordemail" id="forgotpasswordemail" placeholder="Please enter your email address" maxlength="50">
+                            <input class="form-control" type="password" name="old_password" id="old_password" placeholder="Please enter your old password" maxlength="50">
+                            <input class="form-control" type="password" name="new_password" id="new_password" placeholder="Please enter your new password" maxlength="50">
+                            <input class="form-control" type="password" name="new_password2" id="new_password2" placeholder="Please confirm your new password" maxlength="50">
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                       </button>
+                        <input id='forgotpasswordbuttom' class="btn yellow" style="margin: 0; background-color: #fbb034; background-image: linear-gradient(315deg, #fbb034 0%, #ffdd00 74%);" name="signup" type="submit" value="submit">
+                        </button>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </form>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="jscode.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 <?php
-//If user_id or key is missing
-if(!isset($_GET['user_id']) || !isset($_GET['key'])){
-    echo '<div class="alert alert-danger">There was an error. Please click on the link you received by email.</div>'; exit;
+
+$missingEmail = '<p><strong>Please enter your email address!</strong></p>';
+$invalidEmail = '<p><strong>Please enter a valid email address!</strong></p>';
+$missingoldPassword = '<p><strong>Please enter your old Password!</strong></p>';
+$missingnewPassword = '<p><strong>Please enter your new Password!</strong></p>';
+$invalidoldPassword = '<p><strong>Your old password should be at least 6 characters long and inlcude one capital letter and one number!</strong></p>';
+$invalidnewPassword2 = '<p><strong>Your new password should be at least 6 characters long and inlcude one capital letter and one number!</strong></p>';
+$differentPassword = '<p><strong>Passwords don\'t match!</strong></p>';
+$missingPassword2 = '<p><strong>Please confirm your password</strong></p>';
+
+
+
+//    <!--Get username, email, password, password2-->
+//Get email
+
+if(empty($_POST["forgotpasswordemail"])){
+    $errors .= $missingEmail;   
+}else{
+    $email = filter_var($_POST["forgotpasswordemail"], FILTER_SANITIZE_EMAIL);
+    if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+        $errors .= $invalidEmail;   
+    }
 }
-//else
+//Get old passwords
+if(empty($_POST["old_password"])){
+    $errors .= $missingoldPassword; 
+}elseif(!(strlen($_POST["old_password"])>6
+         and preg_match('/[A-Z]/',$_POST["old_password"])
+         and preg_match('/[0-9]/',$_POST["old_password"])
+        )
+       ){
+    $errors .= $invalidoldPassword; 
+}else{
+    $password = filter_var($_POST["old_password"], FILTER_SANITIZE_STRING);}
+
+//Get new passwords
+if(empty($_POST["new_password"])){
+    $errors .= $missingnewPassword; 
+}elseif(!(strlen($_POST["new_password"])>6
+         and preg_match('/[A-Z]/',$_POST["new_password"])
+         and preg_match('/[0-9]/',$_POST["new_password"])
+        )
+       ){
+    $errors .= $invalidPassword2; 
+}else{
+    $password = filter_var($_POST["new_password"], FILTER_SANITIZE_STRING); 
+    if(empty($_POST["new_password2"])){
+        $errors .= $missingPassword2;
+    }else{
+        $password2 = filter_var($_POST["new_password2"], FILTER_SANITIZE_STRING);
+        if($password !== $password2){
+            $errors .= $differentPassword;
+        }
+    }
+}
+//If there are any errors print error
+if($errors){
+    $resultMessage = '<div class="alert alert-danger">' . $errors .'</div>';
+    echo $resultMessage;
+    exit;
+};
+
+
+
+
+
     //Store them in two variables
 $user_id = $_GET['user_id'];
-$key = $_GET['key'];
+$key = $_GET['rkey'];
 $time = time() - 86400;
     //Prepare variables for the query
 $user_id = mysqli_real_escape_string($link, $user_id);
@@ -54,28 +152,11 @@ if(!$result){
 //If combination does not exist
 //show an error message
 $count = mysqli_num_rows($result);
-if($count !== 1){
-    echo '<div class="alert alert-danger">Please try again.</div>';
-    exit;
-}
+//if($count !== 1){
+   // echo '<div class="alert alert-danger">Please try again.</div>';
+  //  exit;
+//}
 //print reset password form with hidden user_id and key fields
-echo "
-<form method=post id='passwordreset'>
-<input type=hidden name=key value=$key>
-<input type=hidden name=user_id value=$user_id>
-<div class='form-group'>
-    <label for='password'>Enter your new Password:</label>
-    <input type='password' name='password' id='password' placeholder='Enter Password' class='form-control'>
-</div>
-<div class='form-group'>
-    <label for='password2'>Re-enter Password::</label>
-    <input type='password' name='password2' id='password2' placeholder='Re-enter Password' class='form-control'>
-</div>
-<input type='submit' name='resetpassword' class='btn btn-success btn-lg' value='Reset Password'>
-
-
-</form>
-";
 
 
 ?>
@@ -88,7 +169,7 @@ echo "
             <!--Script for Ajax Call to storeresetpassword.php which processes form data-->
             <script>
              //Once the form is submitted
-            $("#passwordreset").submit(function(event){ 
+             $("#forgotpasswordbuttom").submit(function(event){ 
                 //prevent default php processing
                 event.preventDefault();
                 //collect user inputs
@@ -96,7 +177,7 @@ echo "
             //    console.log(datatopost);
                 //send them to signup.php using AJAX
                 $.ajax({
-                    url: "storeresetpassword.php",
+                    url: "resetpassword.php",
                     type: "POST",
                     data: datatopost,
                     success: function(data){
