f707dbc78fa07893a84c0471cfe5786346b3343d
alzaibak <alzaibak.abdalaziz@gmail.com>
2021-12-24 09:28:17 +0100

    connection file ignoring

--- addnotes.php
+++ addnotes.php
@@ -3,11 +3,21 @@
 session_start();
 include('connection.php');
 
+$missingvalue = '<p><strong>The value should not be null</strong></p>';
+
+if (!strlen(trim($_GET['textarea']))){
+    $errors .= $missingvalue; 
+}
+
+if ($errors) {
+    $resultMessage = "<div class='alert alert-danger'>$errors</div>";
+    echo $resultMessage; 
+}
 //get user_id
 $user_id = $_SESSION['id'];
 //get the current time
 $time = time();
-$note = $_POST['textarea'];
+$note = $_GET['textarea'];
 $usernote = mysqli_real_escape_string($link, $note);
 //run a query to create new note
 $sql = "INSERT INTO notes (`user_id`, `note`, `time`) VALUES ($user_id, '$usernote', '$time')";
